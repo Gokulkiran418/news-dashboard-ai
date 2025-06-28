3ee3d3a8865d4616279afac78a01ad65b257cef6
Gokulkiran418 <umgokulkiran@gmail.com>
2025-06-28 11:40:13 +0530

    Add types

---
+++ types/article.ts
@@ -0,0 +1,9 @@
+export interface Article {
+  article_id: string;
+  title: string;
+  image_url: string | null;
+  source_id: string;
+  pubDate: string;
+  description: string | null;
+  link: string;
+}
